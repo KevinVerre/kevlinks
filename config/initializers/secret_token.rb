0792571e36f6b5b31e6117ab9cfc5d8b7667b6c6
Kevin Verre <verre.kevin@gmail.com>
2014-04-23 16:13:43 -0700

    Rails new app-name

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kevlinks::Application.config.secret_key_base = 'c7d93a8abf084e590640948fa1b262a9da5c6277ffe2c696b30ec132137e5c3d8a6af9df205172cefd0e858712f58d822b12b39b006ccbc56af7e52c83ba65d0'
